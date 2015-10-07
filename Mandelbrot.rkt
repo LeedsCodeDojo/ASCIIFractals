119aa70b4aaf04d25cb035b2f8d4a38d5fd76b64
Adam Pridmore <adam_pridmore@hotmail.com>
2015-10-07 19:11:24 +0100

    - Initial commit

---
+++ Mandelbrot.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+(define (mandelbrot width height iterations)
+  (define (loopY index)
+    (cond
+      ((> index height) "")
+      (else *))
+  
+  (loopY height)
+)
+
+(mandelbrot 80 40 10)
