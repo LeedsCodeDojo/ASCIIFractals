0c713298ee324d6f057c66b98a678a44f990f2a8
Adam Pridmore <adam_pridmore@hotmail.com>
2015-10-07 19:19:40 +0100

    - Renders a 40 x 80 block of text.

--- Mandelbrot.rkt
+++ Mandelbrot.rkt
@@ -1,12 +1,25 @@
 #lang racket
 
 (define (mandelbrot width height iterations)
+  
+  (define (loopX index)
+    (cond
+      ((<= index 0) "")
+      (else 
+       (string-append (loopX (- index 1)) "*")
+       )))
+  
+  
   (define (loopY index)
     (cond
-      ((> index height) "")
-      (else *))
+      ((<= index 0) "")
+      (else 
+       (string-append (loopY (- index 1)) (loopX width) "\n")
+       )))
   
   (loopY height)
 )
 
-(mandelbrot 80 40 10)
+(display 
+ (mandelbrot 80 40 10)
+ )
